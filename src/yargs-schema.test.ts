da5d21889b6271bb1fadfb81a729d0be5669c173
Mario Nebl <hello@mario-nebl.de>
2019-04-08 09:26:58 +0200

    style: apply format

--- src/yargs-schema.test.ts
+++ src/yargs-schema.test.ts
@@ -5,19 +5,23 @@ const ok = async <T>(p: T): Promise<Ok<T>> => Result.Ok(p).sync();
 const err = async (m: string): Promise<Err> => Result.Err(new Error(m)).sync();
 
 test("accepts empty schema and flags", async () => {
-  const {parse} = configure({});
+  const { parse } = configure({});
   expect(await parse([]).sync()).toEqual(await ok({ _: [] }));
 });
 
 test("empty schema passes flags", async () => {
-  const {parse} = configure({});
-  const result = parse(['-a', '1', '-b', '2', 'something']);
-  expect(await result.sync()).toEqual(await ok({ _: ['something'], a: '1', b: '2' }));
+  const { parse } = configure({});
+  const result = parse(["-a", "1", "-b", "2", "something"]);
+  expect(await result.sync()).toEqual(
+    await ok({ _: ["something"], a: "1", b: "2" })
+  );
 });
 
 // Ajv creates no errors for `additionalProperties` violations
 test.skip("empty schema forbdding additional props returns Err", async () => {
-    const {parse} = configure({ additionalProperties: false });
-    const result = parse(['-a']);
-    expect(await result.sync()).toEqual(await err("Unknown flag -a is not allowed"));
+  const { parse } = configure({ additionalProperties: false });
+  const result = parse(["-a"]);
+  expect(await result.sync()).toEqual(
+    await err("Unknown flag -a is not allowed")
+  );
 });
