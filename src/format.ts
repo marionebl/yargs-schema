1df890921cd8938615b7df52b419b8a957084864
Mario Nebl <hello@mario-nebl.de>
2019-04-13 14:24:18 +0200

    fix: define user validation of positionals

--- src/format.ts
+++ src/format.ts
@@ -1,40 +1,41 @@
 import * as jsonschema from "jsonschema";
 
 export interface Formatable {
-    errors: jsonschema.ValidationError[]
+  errors: jsonschema.ValidationError[];
 }
 
 export function format(formatable: Formatable): string {
-    return formatable.errors
-        .map(error => {
-            return error.property === 'instance._' ? formatPositionalError(error) : formatFlagError(error)
-        })
-        .join('\n');
+  return formatable.errors
+    .map(error => {
+      return error.property === "instance._"
+        ? formatPositionalError(error)
+        : formatFlagError(error);
+    })
+    .join("\n");
 }
 
 function formatFlagError(error: jsonschema.ValidationError): string {
-    switch (error.name) {
-        case 'additionalProperties':
-            return `unknown flag ${formatFlag(error.argument)} is not allowed`;
-        default:
-            return `unknown validation error "${error.name}": ${error.message}`;
-    }
+  switch (error.name) {
+    case "additionalProperties":
+      return `unknown flag ${formatFlag(error.argument)} is not allowed`;
+    default:
+      return `unknown validation error "${error.name}": ${error.message}`;
+  }
 }
 
 function formatPositionalError(error: jsonschema.ValidationError): string {
-    switch (error.name) {
-        case 'items':
-            const mulitple = Array.isArray(error.argument) && error.argument.length > 0;
-            const subject = mulitple ? 'positionals' : 'positional';
-            const verb = mulitple ? 'are' : 'is';
-            return `unknown ${subject} "${error.instance.join(', ')}" ${verb} not allowed`;
-        default:
-            return `unknown validation error "${error.name}": ${error.message}`;
-    }
+  switch (error.name) {
+    case "items":
+      const offending = error.instance.filter((value: unknown) => !jsonschema.validate([value], error.schema).valid);
+      const mulitple = offending.length > 1;
+      const subject = mulitple ? "positionals" : "positional";
+      const verb = mulitple ? "are" : "is";
+      return `unknown ${subject} "${offending.join(", ")}" ${verb} not allowed`;
+    default:
+      return `unknown validation error "${error.name}": ${error.message}`;
+  }
 }
 
 function formatFlag(flag: string) {
-    return flag.length === 1
-        ? `-${flag}`
-        : `--${flag}`;
+  return flag.length === 1 ? `-${flag}` : `--${flag}`;
 }
--- src/yargs-schema.test.ts
+++ src/yargs-schema.test.ts
@@ -2,7 +2,8 @@ import { Result, Ok, Err } from "@marionebl/result";
 import { configure } from "./yargs-schema";
 
 const ok = async <T>(p: T): Promise<Ok<T>> => Result.Ok(p).sync();
-const err = async <T = unknown>(m: string, c?: T): Promise<Err<T>> => Result.Err(m, c).sync();
+const err = async <T = unknown>(m: string, c?: T): Promise<Err<T>> =>
+  Result.Err(m, c).sync();
 
 test("accepts empty schema and flags", async () => {
   const { parse } = configure();
@@ -18,7 +19,7 @@ test("empty schema passes flags", async () => {
 });
 
 test("empty schema forbdding additional props returns Err for -a", async () => {
-  const { parse } = configure({ schema: { additionalProperties: false } });
+  const { parse } = configure({ schema: { additionalProperties: false } });
   const result = parse(["-a"]);
   expect(await result.sync()).toEqual(
     await err("unknown flag -a is not allowed")
@@ -26,9 +27,61 @@ test("empty schema forbdding additional props returns Err for -a", async () => {
 });
 
 test("empty schema forbdding additional props returns Err for input", async () => {
-  const { parse } = configure({ schema: { additionalProperties: false } });
+  const { parse } = configure({ schema: { additionalProperties: false } });
   const result = parse(["a"]);
   expect(await result.sync()).toEqual(
-    await err("unknown positional \"a\" is not allowed")
+    await err('unknown positional "a" is not allowed')
+  );
+});
+
+test("schema defining _ items returns Ok for valid input", async () => {
+  const { parse } = configure({
+    schema: {
+      properties: {
+        _: { type: "array", items: [{ type: "string", enum: ["a", "b", "c"], additionalItems: false }] }
+      }
+    }
+  });
+  const result = parse(["a", "b", "c"]);
+  expect(await result.sync()).toEqual(
+    await ok({
+      _: [
+        "a",
+        "b",
+        "c"
+      ]
+    })
+  );
+});
+
+test("schema defining _ items returns Error for single invalid input", async () => {
+  const { parse } = configure({
+    schema: {
+      properties: {
+        _: { type: "array", items: [{ type: "string", enum: ["a", "b", "c"] }], additionalItems: false }
+      }
+    }
+  });
+  
+  const result = parse(["a", "b", "c", "d"]);
+
+  expect(await result.sync()).toEqual(
+    await err('unknown positional "d" is not allowed')
+  );
+});
+
+test("schema defining _ items returns Error for multiple invalid inputs", async () => {
+  const { parse } = configure({
+    schema: {
+      properties: {
+        _: { type: "array", items: [{ type: "string", enum: ["a", "b", "c"] }], additionalItems: false }
+      }
+    }
+  });
+  
+  const result = parse(["a", "0", "b", "c", "d"]);
+
+  expect(await result.sync()).toEqual(
+    await err('unknown positionals "0, d" are not allowed')
   );
 });
